364c12d493c8ecd4dcbfd75fa0d75282bbdfdfa4
hagan23 <haganemmanuel23@gmail.com>
2023-06-16 09:57:27 +0000

    update

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,39 +1,91 @@
-import React, { useState } from 'react';
-import Button from './Button';
+import { useState } from 'react';
+import calculate from './logic/calculate';
 import './calculator.css';
 
 function Calculator() {
-  const [value, setValue] = useState(0);
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const clickHandler = (e) => {
+    const result = calculate(state, e.target.textContent);
+    setState((prevState) => ({ ...prevState, ...result }));
+  };
+  const { total, next } = state;
 
   return (
-    <div className="container">
-      <div className="output">{value}</div>
-      <div className="buttons">
-        <Button type="button" label="AC" />
-        <Button type="button" label="+/-" />
-        <Button type="button" label="%" />
-        <Button type="button" className="operator" label="÷" />
-        <Button type="button" label="7" />
-        <Button type="button" label="8" />
-        <Button type="button" label="9" />
-        <Button type="button" className="operator" label="x" />
-        <Button type="button" label="4" />
-        <Button type="button" label="5" />
-        <Button type="button" label="6" />
-        <Button type="button" className="operator" label="-" />
-        <Button type="button" label="1" />
-        <Button type="button" label="2" />
-        <Button type="button" label="3" />
-        <Button type="button" className="operator" label="+" />
-        <Button type="button" className="span-2" label="0" />
-        <Button type="button" label="." />
-        <Button
-          type="button"
-          className="operator"
-          label="="
-          onClick={() => setValue(value)}
-        />
-      </div>
+    <div className="calculator">
+      <p className="calc-result">{next || total || 0}</p>
+      <ul className="buttons-container">
+        <li className="button-row">
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            AC
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            +/-
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            %
+          </button>
+          <button className="calc-btn btn-operation" type="button" onClick={clickHandler}>
+            &#247;
+          </button>
+        </li>
+        <li className="button-row">
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            7
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            8
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            9
+          </button>
+          <button className="calc-btn btn-operation" type="button" onClick={clickHandler}>
+            x
+          </button>
+        </li>
+        <li className="button-row">
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            4
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            5
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            6
+          </button>
+          <button className="calc-btn btn-operation" type="button" onClick={clickHandler}>
+            -
+          </button>
+        </li>
+        <li className="button-row">
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            3
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            2
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            1
+          </button>
+          <button className="calc-btn btn-operation" type="button" onClick={clickHandler}>
+            +
+          </button>
+        </li>
+        <li className="button-row">
+          <button className="calc-btn btn-zero" type="button" onClick={clickHandler}>
+            0
+          </button>
+          <button className="calc-btn" type="button" onClick={clickHandler}>
+            .
+          </button>
+          <button className="calc-btn btn-operation" type="button" onClick={clickHandler}>
+            =
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
